9d0f653bdb704936cebc8627cd1a071726b73997
Shivang Dubey <shivangdubey9900@gmail.com>
2021-09-13 23:44:23 +0530

    Added more screenshots to Readme

--- main.js
+++ main.js
@@ -93,7 +93,7 @@ function clickTile(color) {
     }
 
     if (playerSeq.length === compSeq.length) {
-        if (playerSeq.length === 2) {
+        if (playerSeq.length === 20) {
             resetGame(' ~( ˘▾˘~) Congrats! You completed all the levels');
             return
         }
